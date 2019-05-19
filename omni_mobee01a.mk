4bd28b87688dd3fc92540f4f5b023379066f2997
Gautam Maini <gautammaini9@gmail.com>
2019-05-19 19:39:40 +0000

    Omnify

--- omni_mobee01a.mk
+++ omni_mobee01a.mk
@@ -16,12 +16,12 @@
 $(call inherit-product, device/LYF/mobee01a/full_mobee01a.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
-PRODUCT_NAME := lineage_mobee01a
+PRODUCT_NAME := omni_mobee01a
 BOARD_VENDOR := LYF
 PRODUCT_DEVICE := mobee01a
 
