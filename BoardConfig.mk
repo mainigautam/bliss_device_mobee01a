d24fe335e7f0f9696edbfe3a30424d7e530ab759
Gautam <gautammaini9@gmail.com>
2019-05-19 18:48:06 +0000

    Stable Bliss Compiling Now <Build-1-Release>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@
 #
 
 # Inherit from msm8916-common
-include device/cyanogen/msm8916-common/BoardConfigCommon.mk
+include device/LYF/msm8916-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/LYF/mobee01a
 
